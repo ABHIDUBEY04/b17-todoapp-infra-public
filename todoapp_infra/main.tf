646d55969e679052ccfce939fa57175b64932f5f
Abhishek Dubey <nittprincedubey@gmail.com>
2025-09-03 17:08:45 +0530

    Update main.tf

--- todoapp_infra/main.tf
+++ todoapp_infra/main.tf
@@ -23,6 +23,15 @@ module "resource_group1" {
   resource_group_location = "centralindia"
 }
 
+# This RG aanada need to add
+
+module "resource_group1" {
+  source                  = "../modules/azurerm_resource_group"
+  resource_group_name     = "rg-canada"
+  resource_group_location = "canadacentral1"
+}
+
+
 module "virtual_network" {
   depends_on = [module.resource_group]
   source     = "../modules/azurerm_virtual_network"
